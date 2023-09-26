a417ac38fcc85d5fc395957cd8f7ab24d5f4e75c
luattruong2908 <luattruong2908@gmail.com>
2023-09-26 07:45:03 +0700

    add description

--- main.py
+++ main.py
@@ -4,20 +4,26 @@ import pickle
 from time import sleep
 
 waittime = random.uniform(3, 5)
-driver = webdriver.Firefox()
+driver = webdriver.Firefox() #use firefox
 driver.get('https://www.facebook.com')
 
 sleep(waittime)
 
+#use cookies file collecting by getcookie.py
 try:
     with open('cookies.pkl', 'rb') as file:
         cookies = pickle.load(file)
     for cookie in cookies:
         print(cookie)
         driver.add_cookie(cookie)
-
 except:
    print("can't dump cookie")
+   
+# use cookies you copy by hand
+# try:
+#     driver.add_cookie({"name": name, "value": value})
+# except:
+#     print("can't dump cookie")
 
 sleep(waittime)
 
